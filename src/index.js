dc6149b6f84aa244ebbb6da75602aa068fd39ac6
aMinguezg <adrifiupm@gmail.com>
2022-07-01 21:44:47 +0200

    Add serviceWorkers for working offline

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 import './index.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -12,6 +13,8 @@ root.render(
         <App />
     </React.StrictMode>
 )
+
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
